da0c84853213198fccf6419e721a643fcde3b623
Leandro Sizilio <127453291+LeandroSizilio@users.noreply.github.com>
2024-12-06 12:35:22 +0000

    Fix: Correção dos test Jest da função Fatorial

--- fatorial.test.ts
+++ fatorial.test.ts
@@ -8,3 +8,10 @@ test('fatorial de 7', () => {
   expect(fatorial(7)).toBe(5040);
 });
 
+test('fatorial de 0', () => {
+  expect(fatorial(0)).toBe(1);
+});
+
+test('Fatorial -5', () => {
+  expect(() => {fatorial(-5);}).toThrow("Número negativo não é permitido");
+});
--- fatorial.ts
+++ fatorial.ts
@@ -1,6 +1,13 @@
+fatorial(1)
+
 function fatorial(x: number): number {
-    if (x === 1)
+    if ( x == 0 )
         return 1;
+
+    else if (x == 1)
+        return 1;
+    else if (x < 0)
+        throw new Error("Número negativo não é permitido.");
     else 
         return x * fatorial(x - 1);
 }
