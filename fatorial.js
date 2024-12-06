d752adcdc677365278c5b59bbbf5e29472f0bfab
Leandro Sizilio <127453291+LeandroSizilio@users.noreply.github.com>
2024-12-06 04:19:30 +0000

    Fix: Erro no teste por conter arquivos .js

--- fatorial.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function fatorial(x) {
-    if (x === 1)
-        return 1;
-    else
-        return x * fatorial(x - 1);
-}
-exports.default = fatorial;
